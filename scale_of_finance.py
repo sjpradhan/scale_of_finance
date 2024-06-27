4e8d78644afd2c3a8dccfb536126a0d1073c99bc
sjpradhan <104523422+sjpradhan@users.noreply.github.com>
2024-06-27 16:58:40 +0530

    update

--- scale_of_finance.py
+++ scale_of_finance.py
@@ -4,7 +4,6 @@ import streamlit as st
 
 def main():
 
-
     profile_icon = "https://raw.githubusercontent.com/sjpradhan/scale_of_finance/master/salary.png"
 
     st.set_page_config(page_title="SOF-Upload",page_icon = profile_icon)
@@ -15,21 +14,20 @@ def main():
 
     with col1:
         # File uploader
-        uploaded_file = st.file_uploader(":orange[Choose an Excel file] 📁", type=["xlsx", "xls"])
+        uploaded_file = st.file_uploader(":orange[Choose an Excel file] 📁", type=["xlsx", "xls","csv","pdf","txt"])
 
     try:
         if uploaded_file is not None:
-
             def read_from_uploaded_file ():
                 df = pd.read_excel(uploaded_file)
                 df['SOF Amount'].fillna(0, inplace=True)
                 df['Numeric Value of Area Unit'] = 1
                 return df
             df = read_from_uploaded_file ()
+        else:
+            st.write("Please upload file to get insight ⚡")
     except Exception as e:
         st.error("Wrong file, please upload appropriate file with correct header.")
-    else:
-        st.write("Please upload file to get insight ⚡")
 
     try:
         unique_state_name = pd.DataFrame(df['State Name'].unique(),columns= ['State Name'])
@@ -77,8 +75,8 @@ def main():
         st.write(df.isnull().sum().to_frame(name='Blank Values').transpose())
 
         blank_value_position = df[df[['District', 'District Code', 'Agroclimatic Zone', 'Cropping Season',
-                                             'Land Type', 'Area Unit Type', 'Area of Unit', 'Crop Type', 'Crop Name',
-                                             'Crop Name Local Language', 'Scale of Finance In Rs']].isnull().any(axis=1)]
+                                      'Land Type', 'Area Unit Type', 'Area of Unit', 'Crop Type', 'Crop Name',
+                                      'Crop Name Local Language', 'Scale of Finance In Rs']].isnull().any(axis=1)]
         st.dataframe(blank_value_position)
 
         col1, col2 = st.columns(2)
@@ -90,18 +88,22 @@ def main():
             st.table(unique_distrcts)
             st.write(":orange[Total Number Of Unique Districts]", unique_distrcts.shape)
 
-
         with col2:
             @st.cache_data
             def load_district_data():
                 district_url = "https://media.githubusercontent.com/media/sjpradhan/lgd_hierarchy/main/Data/District%20Details.csv"
                 district_df = pd.read_csv(district_url)
-
                 district_df = district_df[
                     ["District LGD Code", "District Name (In English)", "District Name (In Local language)",
-                     "Hierarchy", "Short Name of District", "Census2011 Code", "Pesa Status"]]
+                     "Hierarchy"]].rename(columns={"District LGD Code": "District Code",
+                                                   "District Name (In English)": "District Name",
+                                                   "District Name (In Local language)": "District Local Name",
+                                                   "Hierarchy": "State Name"})
+
+                district_df['State Name'] = district_df['State Name'].str.extract(r'^(.*)\(State\)$')[0]
 
                 return district_df
+
 
             district_df = load_district_data()
 
@@ -110,9 +112,9 @@ def main():
             # Filter based on District Name & it's LGD code
             if search_term:
                 filtered_records = district_df[
-                    district_df['District Name (In English)'].str.contains(search_term, case=False) |
-                    district_df['District LGD Code'].astype(str).str.contains(search_term) |
-                    district_df['Hierarchy'].str.contains(search_term, case=False)
+                    district_df["District Name"].str.contains(search_term, case=False) |
+                    district_df["District Code"].astype(str).str.contains(search_term) |
+                    district_df["State Name"].str.contains(search_term, case=False)
                     ]
 
                 # If there is invalid search it will show no matching found
@@ -121,7 +123,6 @@ def main():
                     st.write(filtered_records)
                 else:
                     st.write(":red[Opps! No matching results found.]🤦‍♂️")
-
 
         st.divider()
 
@@ -151,56 +152,56 @@ def main():
             st.table(actuale_agroclimate_zone)
 
         with col2:
-            unique_agroclimatic_zone = pd.DataFrame(df['Agroclimatic Zone'].unique(),columns=  ['Agroclimatic Zone'])
+            unique_agroclimatic_zone = pd.DataFrame(df['Agroclimatic Zone'].unique(), columns=['Agroclimatic Zone'])
             st.table(unique_agroclimatic_zone)
 
         col1, col2 = st.columns(2)
         with col1:
-            data = ["Rabi Crop (Nov-February)","Kharif Crop (July-Oct)","Summer Crop (March-June)",
-                    "Perennial Crop (Annual Crop)","Any Season Crop","Any",
+            data = ["Rabi Crop (Nov-February)", "Kharif Crop (July-Oct)", "Summer Crop (March-June)",
+                    "Perennial Crop (Annual Crop)", "Any Season Crop", "Any",
                     ]
-            actuale_cropping_season = pd.DataFrame(data,columns = ['Actual Cropping Season'])
+            actuale_cropping_season = pd.DataFrame(data, columns=['Actual Cropping Season'])
             st.table(actuale_cropping_season)
 
         with col2:
-            unique_crop_season = pd.DataFrame(df['Cropping Season'].unique(),columns= ['Cropping Season'])
+            unique_crop_season = pd.DataFrame(df['Cropping Season'].unique(), columns=['Cropping Season'])
             st.table(unique_crop_season)
 
-        col1, col2,col3,col4 = st.columns(4)
+        col1, col2, col3, col4 = st.columns(4)
 
         with col1:
-            data = ["Irrigated Land","Un-Irrigated Land","Any",
+            data = ["Irrigated Land", "Un-Irrigated Land", "Any",
                     ]
-            actuale_land_type = pd.DataFrame(data,columns = ['Actual Land Type'])
+            actuale_land_type = pd.DataFrame(data, columns=['Actual Land Type'])
             st.table(actuale_land_type)
 
         with col2:
-            unique_land_type = pd.DataFrame(df['Land Type'].unique(),columns= ['Land Type'])
+            unique_land_type = pd.DataFrame(df['Land Type'].unique(), columns=['Land Type'])
             st.table(unique_land_type)
 
         with col3:
-            data = ["Square Feet","Square Meter","Square Yard","Hectare","Bhigha","Acre","Guntha","Ground","Biswa",
-                "Kanal","Are","Cent",
+            data = ["Square Feet", "Square Meter", "Square Yard", "Hectare", "Bhigha", "Acre", "Guntha", "Ground", "Biswa",
+                    "Kanal", "Are", "Cent",
                     ]
-            actuale_area_unit_type = pd.DataFrame(data,columns = ["Actual Area Unit Type"])
+            actuale_area_unit_type = pd.DataFrame(data, columns=["Actual Area Unit Type"])
             st.table(actuale_area_unit_type)
 
         with col4:
-            unique_area_unit_type = pd.DataFrame(df['Area Unit Type'].unique(),columns=["Area Unit Type"])
+            unique_area_unit_type = pd.DataFrame(df['Area Unit Type'].unique(), columns=["Area Unit Type"])
             st.table(unique_area_unit_type)
 
-        col1,col2 = st.columns(2)
+        col1, col2 = st.columns(2)
 
-        with col1 :
-            data = ["Cereals","Seed","Pulses","Fruits","Vegetable","Spices","Feed & Forage",
-                    "Oil Seed","Ornamental","Industrial/Commercial/Cash English","Sericulture",
-                    "Medicinal/Herbs","Mix Crop","Inter Crop","Any","Floriculture","Millet"
+        with col1:
+            data = ["Cereals", "Seed", "Pulses", "Fruits", "Vegetable", "Spices", "Feed & Forage",
+                    "Oil Seed", "Ornamental", "Industrial/Commercial/Cash English", "Sericulture",
+                    "Medicinal/Herbs", "Mix Crop", "Inter Crop", "Any", "Floriculture", "Millet"
                     ]
-            actual_crop_type = pd.DataFrame(data, columns = ['Actual Crop Type'])
+            actual_crop_type = pd.DataFrame(data, columns=['Actual Crop Type'])
             st.table(actual_crop_type)
 
-        with col2 :
-            unique_crop_type = pd.DataFrame(df['Crop Type'].unique(), columns= ['Crop Type'])
+        with col2:
+            unique_crop_type = pd.DataFrame(df['Crop Type'].unique(), columns=['Crop Type'])
             st.table(unique_crop_type)
     except:
         pass
